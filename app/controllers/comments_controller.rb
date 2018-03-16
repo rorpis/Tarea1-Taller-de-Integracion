4903909e634a381be9ea024cfecc86066341b6ed
rorpis <r.orpis@gmail.com>
2018-03-16 13:23:40 -0300

    agregado nombre en comentarios

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -72,6 +72,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:submission_id, :body, :user_id)
+      params.require(:comment).permit(:submission_id, :body, :user_id, :title) #111
     end
 end
